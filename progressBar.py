f0d21447632e0b45cca5a859cbac05b3bff3fa3b
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-02-17 22:14:00 +0100

    Update progressBar.py

--- progressBar.py
+++ progressBar.py
@@ -1,17 +1,35 @@
 import tkinter as tk
 from tkinter import ttk
 
-
 class ProgressBar:
-    def __init__(self, master, max_value):
+    def __init__(self, master, title, progress_title, progress_var):
         self.master = master
-        self.progress_bar = None
-        self.max_value = max_value
+        self.title = title
+        self.progress_title = progress_title
+        self.progress_var = progress_var
 
-    def create_progress_bar(self):
-        self.progress_bar = ttk.Progressbar(self.master, orient="horizontal", length=400, mode="determinate", maximum=self.max_value)
-        self.progress_bar.pack(pady=10)
+        self.create_window()
 
-    def update_progress_bar(self, value):
-        self.progress_bar["value"] = value
-        self.master.update_idletasks()
+    def create_window(self):
+        # Create window
+        self.window = tk.Toplevel(self.master)
+        self.window.title(self.title)
+        self.window.geometry("300x100")
+
+        # Create progress bar
+        self.progress_label = ttk.Label(self.window, text=self.progress_title)
+        self.progress_bar = ttk.Progressbar(self.window, variable=self.progress_var, maximum=100)
+        self.progress_label.pack(pady=10)
+        self.progress_bar.pack(pady=5)
+
+    def start(self):
+        self.window.grab_set()
+        self.progress_var.set(0)
+
+    def update(self, progress):
+        self.progress_var.set(progress)
+        self.window.update()
+
+    def end(self):
+        self.progress_var.set(100)
+        self.window.after(500, self.window.destroy)
