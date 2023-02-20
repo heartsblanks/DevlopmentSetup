78f77b4c82f971f4022d23eaf2fc4013ce1e5e1a
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-02-20 15:23:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import tkinter as tk
+import logging
 from gui import InstallOrchestrationGUI
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 def main():
-    root = tk.Tk()
-    app = InstallOrchestrationGUI(root)
-    root.mainloop()
-
+    try:
+        root = tk.Tk()
+        app = InstallOrchestrationGUI(root)
+        root.mainloop()
+    except Exception as e:
+        logging.exception("Unhandled exception occurred: %s", e)
 
 if __name__ == "__main__":
     main()
