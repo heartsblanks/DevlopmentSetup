c74a06cbedad14b48dc32f389586fa7ad18510c5
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-02-20 15:21:54 +0100

    Update installation.py

--- installation.py
+++ installation.py
@@ -5,11 +5,11 @@ from PluginInstaller import PluginInstaller
 from CheckConnections import CheckConnections
 from CreateVariables import CreateVariables
 
+
 class Installation:
     def __init__(self, system_type, install_type):
         self.system_type = system_type
         self.install_type = install_type
-
         self.logger = logging.getLogger(__name__)
         self.checkout_projects = CheckOutProjects(self.install_type, self.logger)
         self.plugin_installer = PluginInstaller(self.install_type, self.logger)
@@ -18,10 +18,13 @@ class Installation:
         self.orchestrate_installation()
 
     def orchestrate_installation(self):
-        self.checkout_projects.run()
-        self.plugin_installer.run()
-        self.check_connections.run()
-        self.create_variables.create()
-
-        messagebox.showinfo("Installation Complete", f"{self.install_type} installation complete.")
-        self.logger.info(f"{self.install_type} installation complete.")
+        try:
+            self.checkout_projects.run()
+            self.plugin_installer.run()
+            self.check_connections.run()
+            self.create_variables.create()
+            messagebox.showinfo("Installation Complete", f"{self.install_type} installation complete.")
+            self.logger.info(f"{self.install_type} installation complete.")
+        except Exception as e:
+            self.logger.exception("An error occurred during installation")
+            messagebox.showerror("Installation Error", f"An error occurred during {self.install_type} installation. Error message: {str(e)}")
