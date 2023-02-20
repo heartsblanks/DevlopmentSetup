15674781f52221b12c8c243db021b198ad7261cc
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-02-20 12:13:43 +0100

    Update configureToolkit.py

--- configureToolkit.py
+++ configureToolkit.py
@@ -1,55 +1,78 @@
+import logging
 import os
 import shutil
-import logging
-from zipfile import ZipFile
-from GenericFunctions import replace_strings_in_file
+from GenericFunctions import GenericFunctions
 
 class ConfigureToolkit:
-    def __init__(self, workspace_dir, toolkit_plugins_dir, toolkit_settings_dir, cvs_settings_file, jdt_settings_file):
-        self.WORKSPACE_DIR = workspace_dir
-        self.TOOLKIT_PLUGINS_DIR = toolkit_plugins_dir
-        self.TOOLKIT_SETTINGS_DIR = toolkit_settings_dir
-        self.CVS_SETTINGS_FILE = cvs_settings_file
-        self.JDT_SETTINGS_FILE = jdt_settings_file
+    def __init__(self, install_type, iib_base_path, workspace_dir, node, user_id, log_file="configure_toolkit.log"):
+        self.install_type = install_type
+        self.iib_base_path = iib_base_path
+        self.workspace_dir = workspace_dir
+        self.node = node
+        self.user_id = user_id
+        self.log_file = log_file
         self.logger = logging.getLogger(__name__)
+        
+        self.metadata_dir = os.path.join(workspace_dir, ".metadata")
+        self.plugins_dir = os.path.join(self.metadata_dir, ".plugins", "com.ibm.etools.mft.broker.runtime")
+        
+        self.version_file = os.path.join(self.iib_base_path, "version.ini")
+        self.lock_file = os.path.join(self.iib_base_path, ".lock")
+        self.settings_dir = os.path.join(self.iib_base_path, "ToolkitSettings")
+        self.plugins_source_dir = os.path.join(self.settings_dir, "plugins")
+        self.settings_source_dir = os.path.join(self.settings_dir, "org.eclipse.core.runtime")
+        self.cvs_settings_file = os.path.join(self.settings_dir, "cvs_settings.xml")
+        self.jdt_settings_file = os.path.join(self.settings_dir, "org.eclipse.jdt.core.prefs")
+        self.mssql_driver_source = os.path.join(self.settings_dir, "MS_SQL_DRIVER_SOURCE.zip")
+        self.mssql_driver_target = os.path.join(self.iib_base_path, "jdbc", "drivers", "Microsoft_SQL_Server")
+        self.dev_broker_file = os.path.join(self.iib_base_path, "IntegrationNodes", install_type, "DEV.broker")
+        self.test_broker_file = os.path.join(self.iib_base_path, "IntegrationNodes", install_type, "TEST.broker")
 
-    def configure(self, iib_basepath):
-        try:
-            # Update eclipse.ini to increase java heap size
-            eclipse_ini_file = os.path.join(iib_basepath, "tools", "eclipse.ini")
-            replace_strings_in_file(eclipse_ini_file, {"-Xmx1024m": "-Xmx4096m"})
-            self.logger.info(f"Updated {eclipse_ini_file} to increase java heap size.")
+    def configure_toolkit(self):
+        logging.basicConfig(filename=self.log_file, level=logging.DEBUG)
+        self.copy_files()
+        self.replace_dev_broker()
+        self.replace_test_broker()
+        GenericFunctions.replace_strings_in_file(self.version_file, {"IIBTK_VERSION=": f"IIBTK_VERSION={self.get_iib_version()}"})
+        self.logger.info("Finished configuring IIB Toolkit.")
+    
+    def copy_files(self):
+        source_target_list = [
+            (self.version_file, os.path.join(self.metadata_dir, "version.ini")),
+            (self.lock_file, os.path.join(self.metadata_dir, ".lock")),
+            (self.cvs_settings_file, self.cvs_settings_file),
+            (self.jdt_settings_file, self.jdt_settings_file),
+            (self.mssql_driver_source, self.mssql_driver_target),
+        ]
+        for source, target in source_target_list:
+            shutil.copy(source, target)
+            self.logger.info(f"Copied {source} to {target}.")
+        
+        for plugin_file in os.listdir(self.plugins_source_dir):
+            source = os.path.join(self.plugins_source_dir, plugin_file)
+            target = os.path.join(self.plugins_dir, plugin_file)
+            shutil.copy(source, target)
+            self.logger.info(f"Copied {source} to {target}.")
+    
+        for settings_file in os.listdir(self.settings_source_dir):
+            source = os.path.join(self.settings_source_dir, settings_file)
+            target = os.path.join(self.iib_base_path, "ToolkitSettings", "org.eclipse.core.runtime", settings_file)
+            shutil.copy(source, target)
+            self.logger.info(f"Copied {source} to {target}.")
+    
+        shutil.copy(self.dev_broker_file, self.plugins_dir)
+        self.logger.info(f"Copied {self.dev_broker_file} to {self.plugins_dir}.")
+        GenericFunctions.replace_strings_in_file(self.plugins_dir + "/DEV.broker", {
+        "NODE": self.node,
+        "LISTEN_PORT": str(self.WEBADMINPORT_SSL),
+        "User": os.getenv("USER_ID")
+        })
+        self.logger.info("Replaced variables in DEV.broker.")
 
-            # Copy files from metadata and plugins directories
-            metadata_dir = os.path.join(iib_basepath, "tools", "eclipse", "plugins", "com.ibm.etools.msgbroker.toolkit_")
-            version_file = os.path.join(metadata_dir, "version.ini")
-            lock_file = os.path.join(metadata_dir, ".lock")
-            shutil.copy(version_file, os.path.join(self.WORKSPACE_DIR, ".metadata"))
-            shutil.copy(lock_file, os.path.join(self.WORKSPACE_DIR, ".metadata"))
-            self.logger.info(f"Copied {version_file} and {lock_file} to {self.WORKSPACE_DIR}/.metadata")
-            
-            plugins_dir = os.path.join(iib_basepath, "tools", "eclipse", "plugins")
-            shutil.copytree(os.path.join(plugins_dir, "com.ibm.etools.msgbroker.toolkit_"), self.TOOLKIT_PLUGINS_DIR)
-            self.logger.info(f"Copied files from {plugins_dir}/com.ibm.etools.msgbroker.toolkit_ to {self.TOOLKIT_PLUGINS_DIR}")
-            
-            settings_dir = os.path.join(iib_basepath, "tools", "eclipse", "configuration", "org.eclipse.core.runtime")
-            shutil.copytree(settings_dir, self.TOOLKIT_SETTINGS_DIR)
-            self.logger.info(f"Copied files from {settings_dir} to {self.TOOLKIT_SETTINGS_DIR}")
-            
-            # Update CVS and JDT settings files
-            replace_strings_in_file(self.CVS_SETTINGS_FILE, {"RPL_USERID": os.environ["USERNAME"]})
-            self.logger.info(f"Updated {self.CVS_SETTINGS_FILE} with USERNAME environment variable.")
-            
-            replace_strings_in_file(self.JDT_SETTINGS_FILE, {"RPL_VERSIONNO": os.environ["IIBTK_VERSION"]})
-            self.logger.info(f"Updated {self.JDT_SETTINGS_FILE} with IIBTK_VERSION environment variable.")
-            
-            # Extract MS SQL driver
-            driver_source = "MS_SQL_DRIVER_SOURCE.zip"
-            driver_target = "MS_SQL_DRIVER_TARGET"
-            with ZipFile(driver_source, 'r') as zipObj:
-                zipObj.extractall(driver_target)
-            self.logger.info(f"Extracted MS SQL driver from {driver_source} to {driver_target}.")
-            
-        except Exception as e:
-            self.logger.exception(f"Error while configuring toolkit: {str(e)}")
-            raise e
+        shutil.copy(self.test_broker_file, self.plugins_dir)
+        self.logger.info(f"Copied {self.test_broker_file} to {self.plugins_dir}.")
+        GenericFunctions.replace_strings_in_file(self.plugins_dir + "/TEST.broker", {
+        "User": os.getenv("USER_ID")
+    })
+    self.logger.info("Replaced variables in TEST.broker.")
+
