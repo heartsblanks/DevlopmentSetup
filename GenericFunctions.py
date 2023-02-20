084e270e31c5c5d103091cd8dda343310cbced53
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-02-20 11:38:46 +0100

    Update GenericFunctions.py

--- GenericFunctions.py
+++ GenericFunctions.py
@@ -19,7 +19,33 @@ class GenericFunctions:
         except Exception as e:
             logger.error(f"Error replacing strings in file {file_path}: {str(e)}")
 
+    def get_developer_ports(self, ssh_host, ssh_port, ssh_username, ssh_password):
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(ssh_host, port=ssh_port, username=ssh_username, password=ssh_password)
 
+        try:
+            stdin, stdout, stderr = ssh.exec_command("mqsilist -v")
+            output = stdout.read().decode("utf-8")
+            self.HTTP_CONNECTOR = self.HTTPS_CONNECTOR = self.WEBADMINPORT_SSL = self.WEBADMINPORT = self.MQ_LISTENER_PORT = self.DEBUG_PORT = "Not Found"
+            for line in output.splitlines():
+                if "HTTPConnector:" in line:
+                    self.HTTP_CONNECTOR = line.split(":")[1].strip()
+                elif "HTTPSConnector:" in line:
+                    self.HTTPS_CONNECTOR = line.split(":")[1].strip()
+                elif "Webadminport (SSL):" in line:
+                    self.WEBADMINPORT_SSL = line.split(":")[1].strip()
+                elif "Webadminport:" in line:
+                    self.WEBADMINPORT = line.split(":")[1].strip()
+                elif "MQ-Listener Port:" in line:
+                    self.MQ_LISTENER_PORT = line.split(":")[1].strip()
+                elif "Debug Port:" in line:
+                    self.DEBUG_PORT = line.split(":")[1].strip()
+        except Exception as e:
+            self.logger.error(f"Error getting developer ports via SSH: {str(e)}")
+            raise e
+        finally:
+            ssh.close()
             
 #gf = GenericFunctions()
 #replacements = {
