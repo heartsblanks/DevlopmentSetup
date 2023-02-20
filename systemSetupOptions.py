8ac01da8b457a336458f0be84485f84153bd7520
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-02-20 14:31:06 +0100

    Update systemSetupOptions.py

--- systemSetupOptions.py
+++ systemSetupOptions.py
@@ -32,48 +32,68 @@ class SystemSetupOptions:
         self.create_options()
 
     def create_options(self):
-        self.options = {}
-        try:
-            with open(f"config/{self.system_type}_{self.install_type}.txt", "r") as f:
-                for line in f:
-                    key, value = line.split("=")
-                    self.options[key.strip()] = value.strip()
-        except FileNotFoundError:
-            self.logger.warning(f"No configuration file found for {self.system_type} - {self.install_type}.")
+        # Create new top level window
+        self.system_setup_options_window = tk.Toplevel(self.master)
+        self.system_setup_options_window.title(f"{system_type} {install_type} Setup")
 
-        for key, value in self.options.items():
-            label = ttk.Label(self.setup_options_window, text=key, font="Helvetica 10 bold")
-            label.pack(pady=5)
+        # Set up custom styles for this window
+        self.setup_system_setup_options_styles()
 
-            entry = ttk.Entry(self.setup_options_window)
-            entry.insert(0, value)
-            entry.pack(pady=5)
+        # Create labels and options
+        self.installation_type_var = self.create_label_and_options("Unattended installation", ["Yes", "No"])
+        self.repo_update_var = self.create_label_and_options("Repository", ["Update", "Replace"])
+        self.hb_password_entry = self.create_label_and_entry("HB Password")
+        if install_type in ["IIB10", "ACE12"]:
+            self.ho_password_entry = self.create_label_and_entry("HO Password")
+            self.auto_reboot_var = self.create_label_and_options("Reboot Automatically", ["Yes", "No"])
 
-            browse_button = ttk.Button(self.setup_options_window, text="Browse", command=lambda entry=entry: self.browse_for_file(entry))
-            browse_button.pack(pady=5)
+        # Create submit button
+        submit_button = ttk.Button(self.system_setup_options_window, text="Submit", command=lambda: self.submit_options, style="SubmitButton.TButton")
+        submit_button.pack(pady=10)
 
-    def browse_for_file(self, entry):
-        file_path = filedialog.askopenfilename()
-        entry.delete(0, "end")
-        entry.insert(0, file_path)
+        # Create quit button
+        quit_button = ttk.Button(self.system_setup_options_window, text="Quit", command=self.system_setup_options_window.destroy, style="QuitButton.TButton")
+        quit_button.pack(pady=10)
+        self.system_options_window.destroy()
 
-    def quit_options(self):
-        self.setup_options_window.destroy()
-        self.system_options_window.deiconify()
+    def setup_system_setup_options_styles(self):
+        self.system_setup_options_window.option_add("*Button.Background", "#4d4d4d")
+        self.system_setup_options_window.option_add("*Button.Foreground", "white")
+        self.system_setup_options_window.option_add("*Button.activeBackground", "#808080")
+        self.system_setup_options_window.option_add("*Button.activeForeground", "white")
+        self.system_setup_options_window.option_add("*Button.highlightThickness", 0)
+        self.system_setup_options_window.option_add("*Label.Background", "#d9d9d9")
+        self.system_setup_options_window.option_add("*Label.Foreground", "#4d4d4d")
+
+    def create_label_and_options(self, label_text, option_values):
+        label = ttk.Label(self.system_setup_options_window, text=label_text, style="OptionLabel.TLabel")
+        label.pack(pady=5)
+        var = tk.StringVar(value=option_values[0])
+        for option_value in option_values:
+            radio = ttk.Radiobutton(self.system_setup_options_window, text=option_value, variable=var, value=option_value, style="Option.TRadiobutton")
+            radio.pack()
+        return var
+
+    def create_label_and_entry(self, label_text):
+        label = ttk.Label(self.system_setup_options_window, text=label_text, style="OptionLabel.TLabel")
+        label.pack(pady=5)
+        entry = ttk.Entry(self.system_setup_options_window, show="*", style="Option.TEntry")
+        entry.pack()
+        return entry
 
     def submit_options(self):
-        for key, value in self.options.items():
-            if key in ["keystore", "truststore"]:
-                if not os.path.exists(self.options[key]):
-                    self.logger.warning(f"{self.options[key]} not found for {self.install_type}.")
-            elif key == "sslProfile" and value != "":
-                if not os.path.exists(self.options[key]):
-                    self.logger.warning(f"{self.options[key]} not found for {self.install_type}.")
-            self.options[key] = self.setup_options_window.children[key + "!entry"].get()
+        # get the values of the options
+        installation_type = self.installation_type_var.get()
+        repo_update = self.repo_update_var.get()
+        hb_password = self.hb_password_entry.get()
 
-        with open(f"config/{self.system_type}_{self.install_type}.txt", "w") as f:
-            for key, value in self.options.items():
-                f.write(f"{key}={value}\n")
+        if self.install_type in ["IIB10", "ACE12"]:
+            ho_password = self.ho_password_entry.get()
+            auto_reboot = self.auto_reboot_var.get()
+        else:
+            ho_password = None
+            auto_reboot = None
 
-        self.setup_options_window.destroy()
-        self.system_options_window.deiconify()
+        # instantiate the Installation class
+        installation = Installation(self.system_type, self.install_type, self.options['systemPath'])
+
