46133205b6f86d72f332056d815b34ad1e817734
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-03-02 15:07:42 +0100

    Update createVariables.py

--- createVariables.py
+++ createVariables.py
@@ -1,5 +1,5 @@
 import os
-import csv
+import json
 import logging
 from tkinter import messagebox, filedialog
 
@@ -11,28 +11,33 @@ class CreateVariables:
 
     def create(self):
         try:
-            with open("variable_details.csv", "r") as f:
-                reader = csv.DictReader(f)
-                for row in reader:
-                    if row[self.system_type] == "Y":
-                        self.create_variable(row)
+            with open("constants.json", "r") as f:
+                data = json.load(f)
+                for item in data[self.system_type]["Variables"]:
+                    self.create_variable(item)
+                for item in data[self.system_type]["Files"]:
+                    self.create_file(item)
+                for item in data[self.system_type]["Directories"]:
+                    self.create_directory(item)
+                for item in data[self.system_type]["Connection_Details"]:
+                    self.create_connection_detail(item)
         except Exception as e:
-            self.logger.error(f"Error while reading variable_details.csv: {str(e)}")
+            self.logger.error(f"Error while reading constants.json: {str(e)}")
             raise e
 
-    def create_variable(self, row):
-        var_name = row['name']
-        var_value = row['value']
-        update_path = row['updatePath']
-        var_type = row['Type']
+    def create_variable(self, item):
+        var_name = item["Name"]
+        var_value = item["Value"]
+        var_type = item.get("Type", "I")
+        update_path = item.get("Path", "N")
 
         try:
-            if update_path == "True":
+            if update_path == "Y":
                 if os.path.isabs(var_value):
                     if os.path.exists(var_value):
                         os.environ["PATH"] += os.pathsep + var_value
                     else:
-                        self.show_error_message(row)
+                        self.show_error_message(item)
                         self.logger.error(f"Variable {var_name} could not be created. {var_value} is not a valid path.")
                 else:
                     os.environ["PATH"] += os.pathsep + var_value
@@ -43,7 +48,7 @@ class CreateVariables:
                         os.environ[var_name] = var_value
                         setattr(self, var_name, var_value)
                     else:
-                        self.show_error_message(row)
+                        self.show_error_message(item)
                         self.logger.error(f"Variable {var_name} could not be created. {var_value} is not a valid path.")
                 else:
                     os.environ[var_name] = var_value
@@ -54,7 +59,7 @@ class CreateVariables:
                     if os.path.exists(var_value):
                         setattr(self, var_name, var_value)
                     else:
-                        self.show_error_message(row)
+                        self.show_error_message(item)
                         self.logger.error(f"Variable {var_name} could not be created. {var_value} is not a valid path.")
                 else:
                     setattr(self, var_name, var_value)
@@ -63,11 +68,52 @@ class CreateVariables:
             self.logger.error(f"Error while creating variable {var_name}: {str(e)}")
             raise e
 
-    def show_error_message(self, row):
-        message = f"Could not create variable for {row['name']} with value {row['value']}. Please update the variable_details.csv file with a valid path."
-        result = messagebox.askyesno("Error", message)
-        if result:
-            file_path = filedialog.askopenfilename(initialdir=".", title="Select CSV file", filetypes=[("CSV files", "*.csv")])
-            os.startfile(file_path)
-        else:
-            raise Exception(message)
+    def create_file(self, item):
+        source = item.get("Source")
+        target = item.get("Target")
+
+        try:
+            if os.path.exists(source):
+                os.makedirs(os.path.dirname(target), exist_ok=True)
+                os.replace(source, target)
+            else:
+                self.show_error_message(item)
+                self.logger.error(f"File {source} could not be copied to {target}. {source} does not exist.")
+        except Exception as e:
+            self.logger.error(f"Error while copying file {source} to {target}: {str(e)}")
+            raise e
+
+    def create_directory(self, item):
+        dir_path = item.get("Name")
+
+        try:
+            if not os.path.exists(dir_path):
+                os.makedirs(dir_path)
+            else:
+                self.logger.warning(f"Directory {dir_path} already exists.")
+        except Exception as e:
+            self.logger.error(f"Error while creating directory {dir_path}: {str(e)}")
+            raise e
+
+    def create_connection_detail(self, item):
+        name = item.get("Name")
+        value = item.get("Value")
+            try:
+        # Your code to create the connection detail goes here
+        pass
+    except Exception as e:
+        self.logger.error(f"Error while creating connection detail {name}: {str(e)}")
+        raise e
+
+def show_error_message(self, item):
+    message = f"Could not create variable for {item['Name']} with value {item['Value']}. Please update the constants.json file with a valid path."
+    result = messagebox.askyesno("Error", message)
+    if result:
+        file_path = filedialog.askopenfilename(initialdir=".", title="Select JSON file", filetypes=[("JSON files", "*.json")])
+        os.startfile(file_path)
+    else:
+        raise Exception(message)
+
+
+
+
