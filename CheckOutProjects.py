b8d82af4d227edb2863cb366b4198cb2886260a5
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-04-03 10:28:27 +0200

    Update CheckOutProjects.py

--- CheckOutProjects.py
+++ CheckOutProjects.py
@@ -1,3 +1,18 @@
+from original_file import CheckOutProjects, CheckoutError
+
+update_type = "-q"  # Example value for update_type
+workspace_dir = "/path/to/workspace"  # Example value for workspace_dir
+log_file = "checkout.log"  # Example value for log_file
+
+project1 = {"name": "project1", "tags": {"cvs": "tag1", "git": "tag2"}, "folder_name": "project1_folder"}
+project2 = {"name": "project2", "tags": {"cvs": "tag3", "git": "tag4"}, "folder_name": "project2_folder"}
+project3 = {"name": "project3", "tags": {"cvs": None, "git": "tag5"}, "folder_name": "project3_folder"}
+
+checkout = CheckOutProjects(update_type, workspace_dir, log_file)
+project_list = [project1, project2, project3]
+checkout.checkout_projects(project_list)
+
+
 import os
 import subprocess
 import logging
@@ -16,12 +31,18 @@ class CheckOutProjects:
             logging.info(f"Starting checkout process for projects")
 
             for project in project_list:
-                if "name" in project and "tags" in project and "folder_name" in project:
+                if "name" in project and "folder_name" in project:
                     project_name = project["name"]
-                    tags = project["tags"]
                     folder_name = project["folder_name"]
-                    self.checkout_cvs_project(project_name, folder_name, tags["cvs"])
-                    self.checkout_git_project(project_name, folder_name, tags["git"])
+                    
+                    if "cvs" in project.get("tags", {}):
+                        tag = project["tags"]["cvs"]
+                        self.checkout_cvs_project(project_name, folder_name, tag)
+                    elif "git" in project.get("tags", {}):
+                        tag = project["tags"]["git"]
+                        self.checkout_git_project(project_name, folder_name, tag)
+                    else:
+                        self.checkout_latest_project(project_name, folder_name)
                 else:
                     raise ValueError("Invalid project parameters")
 
@@ -51,14 +72,18 @@ class CheckOutProjects:
         except subprocess.CalledProcessError as e:
             logging.error(f"Error checking out {project_name} project: {e}")
             raise CheckoutError(str(e))
+    
+    def checkout_latest_project(self, project_name, folder_name):
+        logging.info(f"Checking out {project_name} project with the latest version")
+        
+        try:
+            cmd = f"cvs {self.update_type} {project_name}"  # Assuming CVS, replace with Git command as needed
+            cwd = os.path.join(self.workspace_dir, folder_name)
+            subprocess.check_output(cmd, shell=True, cwd=cwd)
+        except subprocess.CalledProcessError as e:
+            logging.error(f"Error checking out {project_name} project: {e}")
+            raise CheckoutError(str(e))
 
 
 class CheckoutError(Exception):
     pass
-#project_list = [
-#    {"name": "project1", "folder_name": "proj1", "tags": {"cvs": "tag1", "git": "tag2"}},
-#    {"name": "project2", "folder_name": "proj2", "tags": {"cvs": "tag3", "git": "tag4"}},
-#]
-
-#checkout = CheckOutProjects("update", "/path/to/workspace")
-#checkout.checkout_projects(project_list)
