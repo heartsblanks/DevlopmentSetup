d5eeabc7a1047d2377d1b825eaf626b203e69b7d
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-10-06 15:00:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,50 @@
 import tkinter as tk
 
 def is_row_empty(frame, row):
-    # Check if all four columns in the given row are empty
-    for col in range(frame.columns - 4, frame.columns):
+    # Check if all columns in the given row are empty
+    for col in range(frame.columns):
         widget = frame.grid_slaves(row=row, column=col)
         if widget:
             return False
     return True
 
 def insert_label(frame, row):
-    # Insert a label with text in the last four columns of the given row
+    # Insert a label with text in all columns of the given row
     label = tk.Label(frame, text="Empty Row")
-    label.grid(row=row, column=frame.columns - 4, columnspan=4)
+    label.grid(row=row, column=0, columnspan=frame.columns)
+
+def get_existing_rows(frame):
+    # Determine the number of rows with labels
+    existing_rows = 0
+    for row in range(frame.rows):
+        if not is_row_empty(frame, row):
+            existing_rows += 1
+    return existing_rows
+
+def add_row():
+    existing_rows = get_existing_rows(frame)
+    new_row = existing_rows
+    frame.rows += 1
+
+    # Check if the new row should have a label
+    if is_row_empty(frame, new_row):
+        insert_label(frame, new_row)
 
 root = tk.Tk()
-root.title("Check and Insert Labels")
+root.title("Dynamic Rows")
 
 frame = tk.Frame(root)
 frame.grid(row=0, column=0)
 
-frame.rows = 10  # Number of rows
 frame.columns = 10  # Number of columns
+frame.rows = 0  # Initialize the number of rows
 
-# Insert widgets or data in the frame as needed
+# Create and insert some initial labels (for demonstration)
+insert_label(frame, frame.rows)
+frame.rows += 1
 
-# Check rows in the last four columns and insert labels when appropriate
-for row in range(frame.rows):
-    if is_row_empty(frame, row):
-        insert_label(frame, row)
+# Create a button to add a new row
+add_button = tk.Button(root, text="Add Row", command=add_row)
+add_button.grid(row=1, column=0, pady=10)
 
 root.mainloop()
