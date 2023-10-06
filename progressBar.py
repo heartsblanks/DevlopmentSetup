7aa090ce96d0f07c9a21bf9d4e7ad3a8ac6eaa91
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-10-06 13:31:25 +0200

    Update progressBar.py

--- progressBar.py
+++ progressBar.py
@@ -1,46 +1,36 @@
-import threading
 import tkinter as tk
 from tkinter import ttk
+import threading
+import time
 
 class ProgressBar:
-    def __init__(self, master, max_value, message, size="400x75"):
-        self.master = master
-        self.max_value = max_value
+    def __init__(self, frame, message, row, column):
+        self.frame = frame
         self.message = message
+        self.row = row
+        self.column = column
 
-        self.top_level = tk.Toplevel(master)
-        self.top_level.title("Installation Progress")
-        self.top_level.geometry(size)
-        self.top_level.protocol("WM_DELETE_WINDOW", self.disable_close_button)
+        self.label = ttk.Label(frame, text=self.message)
+        self.label.grid(row=row, column=column, pady=10)
 
-        self.label = ttk.Label(self.top_level, text=self.message)
-        self.label.pack(pady=10)
+        self.progress = ttk.Progressbar(frame, orient="horizontal", length=350, mode="indeterminate")
+        self.progress.grid(row=row + 1, column=column, pady=5)
 
-        self.progress = ttk.Progressbar(self.top_level, orient="horizontal", length=350, mode="determinate", maximum=self.max_value)
-        self.progress.pack()
-
-        self.top_level.grab_set()
-
-    def disable_close_button(self):
-        pass
-
-    def update(self, current_value):
-        self.progress["value"] = current_value
-        self.progress.update()
-
-    def destroy(self):
-        self.top_level.destroy()
+    def update(self, new_message):
+        self.label.config(text=new_message)
 
 def process_data(progress_bar):
     # Perform time-consuming tasks here
-    for i in range(progress_bar.max_value):
-        # Simulating a time-consuming task
-        import time
-        time.sleep(0.1)
-        progress_bar.update(i + 1)
+    for i in range(10):
+        new_message = f"Processing Step {i+1}"
+        progress_bar.update(new_message)
+        time.sleep(1)
 
 def start_processing():
-    progress = ProgressBar(master, max_value=100, message="Processing Data")
+    frame = tk.Frame(master)
+    frame.grid(row=1, column=0, padx=10, pady=10)  # Adjust row and column as needed
+
+    progress = ProgressBar(frame, message="Processing Data", row=0, column=0)  # Specify row and column
 
     # Create a thread for the task
     thread = threading.Thread(target=process_data, args=(progress,))
@@ -53,7 +43,7 @@ master = tk.Tk()
 
 # Button to start the processing
 start_button = ttk.Button(master, text="Start Processing", command=start_processing)
-start_button.pack()
+start_button.grid(row=0, column=0, padx=10, pady=10)
 
 # Run the Tkinter event loop
 master.mainloop()
