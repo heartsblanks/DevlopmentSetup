c2f1b5d142359a1741f76f6c646aac402efc2bb0
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-10-06 13:39:37 +0200

    Update progressBar.py

--- progressBar.py
+++ progressBar.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 import threading
-import time
+from tqdm import tqdm  # Import tqdm
 
 class ProgressBar:
     def __init__(self, frame, message, row, column):
@@ -13,23 +13,19 @@ class ProgressBar:
         self.label = ttk.Label(frame, text=self.message)
         self.label.grid(row=row, column=column, pady=10)
 
-        self.progress = ttk.Progressbar(frame, orient="horizontal", length=350, mode="indeterminate")
-        self.progress.grid(row=row + 1, column=column, pady=5)
-
-    def start(self):
-        self.progress.start()
-
-    def stop(self):
-        self.progress.stop()
-
     def set_message(self, new_message):
         self.label.config(text=new_message)
 
 def process_data(progress_bar):
     # Perform time-consuming tasks here
-    for i in range(10):
-        new_message = f"Processing Step {i+1}"
+    total_iterations = 10
+
+    # Create a tqdm progress bar
+    for i in tqdm(range(total_iterations), desc="Processing", unit="step"):
+        new_message = f"Processing Step {i + 1}"
         progress_bar.set_message(new_message)
+        # Simulate a time-consuming task
+        import time
         time.sleep(1)
 
 def start_processing():
@@ -38,18 +34,11 @@ def start_processing():
 
     progress = ProgressBar(frame, message="Processing Data", row=0, column=0)  # Specify row and column
 
-    # Start the progress bar
-    progress.start()
-
     # Create a thread for the task
     thread = threading.Thread(target=process_data, args=(progress,))
 
     # Start the thread
     thread.start()
-
-    # You can stop the progress bar when the task is completed (optional)
-    thread.join()
-    progress.stop()
 
 # Create the master window
 master = tk.Tk()
